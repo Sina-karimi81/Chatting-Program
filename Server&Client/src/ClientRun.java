2bd1fa527262018abff4113c2c49574f1ad25803
Sina <ksina9279@gmail.com>
2022-03-10 16:00:58 +0330

    fixed the situation where the message wouldn't be sent to the other side

--- Server&Client/src/ClientRun.java
+++ Server&Client/src/ClientRun.java
@@ -2,8 +2,12 @@ import javax.swing.*;
 import java.io.IOException;
 
 public class ClientRun {
-    public static void main(String[] args) throws IOException {
-        Client c = new Client("127.0.0.1");
-        c.Run();
+    public static void main(String[] args) {
+        try{
+            Client c = new Client("127.0.0.1");
+            c.Run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
